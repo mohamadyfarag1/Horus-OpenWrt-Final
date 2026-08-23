4352b26095bfc5a64d7b4c416a2ce9c654101e80
mohamadyfarag1 <95290971+mohamadyfarag1@users.noreply.github.com>
2026-08-23 22:09:11 +0300

    Remove Superchannel: Delete custom regulatory DB and use standard ath10k firmware

--- test.py
+++ test.py
@@ -1,16 +1,1 @@
-
-import re
-content = '''
-                    partition@0 {
-                        label = \
-rootfs\;
-                        reg = <0x0 0x2000000>;
-                    };
-'''
-def patch_rootfs(match):
-    block = match.group(0)
-    block = re.sub(r'0x0*2000000', '0x08000000', block)
-    return block
-res = re.sub(r'(partition@[0-9a-fA-F]+\s*\{[^}]*label\s*=\s*\rootfs\;[^}]*\})', patch_rootfs, content, flags=re.DOTALL)
-print(res)
-
+import paramiko
